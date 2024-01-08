1130754998710e3d63268e14b08c5559677c6881
anhpp0910 <phuonganhphanhhh@gmail.com>
2024-01-08 13:46:32 +0700

    try prettier

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const express = require('express');
-const { engine } = require('express-handlebars');
-const morgan = require('morgan');
+const path = require("path");
+const express = require("express");
+const { engine } = require("express-handlebars");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
-const route = require('./routes');
+const route = require("./routes");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
-    express.urlencoded({
-        extended: true,
-    }),
+  express.urlencoded({
+    extended: true,
+  })
 );
 app.use(express.json());
 
@@ -22,17 +22,18 @@ app.use(express.json());
 
 // Template engine
 app.engine(
-    'hbs',
-    engine({
-        extname: '.hbs',
-    }),
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
 );
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
